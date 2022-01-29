dcc94f19af981e8c22e14604e480162fe350bab0
jay <jayfriso@gmail.com>
2022-01-28 23:21:54 -0800

    Idea to make it so revealed cards stay revealed. Gonna ditch the idea now because it increases scope.

--- proj.draftcraft_models/Model/DraftPile.h
+++ proj.draftcraft_models/Model/DraftPile.h
@@ -1,10 +1,28 @@
 #pragma once
 #include <vector>
+#include <cmath>
 #include "../StaticData/Card.h"
 #include "DraftDeck.h"
 #include "../ViewModel/DispatcherViewModel.h"
 
 using namespace std;
+
+class DraftCard
+{
+private:
+    const Card* m_card;
+    int m_seenFlags;
+public:
+    DraftCard(const Card* card) : m_card{ card }, m_seenFlags{ 0 } {}
+    bool getSeenByPlayer(unsigned int playerIndex) const 
+    {
+        (m_seenFlags & (int)std::exp2(playerIndex)) > 0;
+    }
+    void setSeenByPlayer(unsigned int playerIndex)
+    {
+        m_seenFlags = m_seenFlags | (int)exp2(playerIndex);
+    }
+};
 
 class DraftPile : public DispatcherViewModel<DraftPile>
 {
